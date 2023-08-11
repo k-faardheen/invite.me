ac3447bfea9cba6927ead34d162561009c242d09
muntaswir <muntaswir.mw@gmail.com>
2023-08-11 22:59:46 +0400

    build: event panel

---
+++ src/main/java/com/mycompany/invite/me/InviteMe.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.invite.me;
+
+/**
+ *
+ * @author Admin
+ */
+public class InviteMe {
+
+    public static void main(String[] args) {
+           java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new eventGUI().setVisible(true);
+            }
+        });     
+        
+
+    }
+}
